28c4c03849f5472721866469cf54f741efc931e2
jattoabdul <jattoade@gmail.com>
2019-05-24 07:11:10 +0100

    implement design for home page and about page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="dashboard layout">
-    <nav />
+  <div class="top-level layout">
+    <nav class="container">
+      <nuxt-link to="/">
+        Silas Onmbayugh
+      </nuxt-link>
+      <nuxt-link to="/about">
+        About/Resume
+      </nuxt-link>
+    </nav>
     <main>
       <nuxt />
     </main>
-    <footer />
+    <footer class="container">
+      <div class="content">
+        <p>Be great to hear from you! Drop me a line below.</p>
+        <button class="contact-action">
+          silas@onsilas.com
+        </button>
+      </div>
+      <div class="socials">
+        <a href="#"><img src="/svg/linkedin.svg" alt="linkedin"></a>
+        <a href="#"><img src="/svg/dribbble.svg" alt="dribble"></a>
+        <a href="#"><img src="/svg/twitter.svg" alt="twitter"></a>
+        <a href="#"><img src="/svg/instagram.svg" alt="instagram"></a>
+      </div>
+    </footer>
   </div>
 </template>
 
